42f9cddf4d503cd4c87c3ad654b05df05816586a
chiwonson <166478273+chiwonson@users.noreply.github.com>
2024-06-30 16:38:14 +0900

    Title: 정수 복사, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240630/정수 복사/copy-integer.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3;
+        int b = 4;
+        b = a;
+        int answer = a * b;
+        System.out.print(a+" ");
+        System.out.println(b);
+        System.out.println(answer);
+    }
+}
