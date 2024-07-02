0da3a48192662b99fd1b663c2b50b2c4e0295426
chiwonson <166478273+chiwonson@users.noreply.github.com>
2024-07-02 19:24:33 +0900

    Title: 입력받아 계산 4, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240702/입력받아 계산 4/input-calculate-4.java
+++ 240702/입력받아 계산 4/input-calculate-4.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = 55;
-        a = sc.nextInt() + 55;
+        a = sc.nextInt() * 2;
         System.out.println(a);
     }
 }
