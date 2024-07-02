7df2caf74f16cd077308c868c87e4e1a145da3f5
chiwonson <166478273+chiwonson@users.noreply.github.com>
2024-07-02 19:23:25 +0900

    Title: 정수 입력받아 계산, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240702/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = 4;
+        a = sc.nextInt() * 2 + 3;
+        System.out.println(a);
+    }
+}
