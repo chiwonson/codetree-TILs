ef1a2e18bb6f2ffbcf10c58fc47d797673453144
chiwonson <166478273+chiwonson@users.noreply.github.com>
2024-07-06 20:38:24 +0900

    Title: 세 실수의 반올림, Time: 171ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240706/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+        System.out.printf("%.3f", a);
+        System.out.println();
+
+        System.out.printf("%.3f", b);
+        System.out.println();
+
+        System.out.printf("%.3f", c);
+        System.out.println();
+
+    }
+}
